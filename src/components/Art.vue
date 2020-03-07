fa99cb6db6022dd0d187d608b2a7187da892803f
Raymond Coti <raymondcoti@Raymonds-MacBook-Pro.local>
2020-03-07 12:43:31 -0600

    got art component to work

---
+++ src/components/Art.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-for="image in images" :key="image.src" >
+            <img :src="image.src" />
+        </div>
+    </div>
+</template>
+
+<script >
+
+export default {
+  name: 'ArtGallery',
+  props: {
+    images: Array,
+  },
+};
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{inputText}}</h1>
+    <!-- {{images}} -->
+    <ArtGallery :images="images" />
+    <HelloWorld :message="inputText" />
+    <input v-model="inputText"/>
   </div>
 </template>
 
-<script>
+<script >
 // @ is an alias to /src
+import selfImg from '@/assets/self.png';
+import soulImg from '@/assets/soulSearching.jpg';
+import ArtGallery from '@/components/Art.vue';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
+    ArtGallery,
     HelloWorld,
   },
+  data: () => ({
+    images: [
+      {
+        src: selfImg,
+        title: 'Self',
+      },
+      {
+        src: soulImg,
+        title: 'searching',
+      },
+    ],
+    inputText: 'hi',
+  }),
 };
 </script>
