c8a397de68577007532aa5e79b154f2259a5260b
raycoti <raymond.coti@gmail.com>
2020-05-04 19:42:17 -0500

    adds projects page

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,1 +1,46 @@
+<template>
+  <div class="project"
+    :style="{backgroundImage: 'url('+ project.src + ')'}"
+  >
+  <div>
+    <h1>{{project.title}}</h1>
+  </div>
+  </div>
+</template>
 
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+interface ProjectItem {
+  src: string;
+  title?: string;
+  description?: unknown;
+}
+
+@Component
+export default class Project extends Vue {
+  @Prop() private project!: ProjectItem;
+}
+
+</script>
+
+<style scoped lang="scss">
+.project{
+  // width: 100%;
+  // max-width: 40rem;
+  // max-height: 40rem;
+  background-position: center;
+  background-size: cover;
+  min-height: 25rem;
+  width: 100%;
+  display: flex;
+  div{
+    width: 100%;
+    margin-top: auto;
+    background-color: black;
+    h1{
+      color: #ffffff;
+  }
+  }
+}
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,27 +1,52 @@
 <template>
-  <transition>
+  <!-- <transition> -->
     <div class="projects">
       <Nav />
       <div class="container">
-        <h1>my projects</h1>
+        <Project
+          v-for="project in projects"
+          :key="project.src"
+          :project="project"
+        />
       </div>
     </div>
-  </transition>
+  <!-- </transition> -->
 </template>
 
 <style scoped lang="scss">
 .container{
+  width: 100%;
   margin-top: 5rem;
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(2, 1fr);
+  // grid-template-rows: repeat(2, 1fr);
 }
 </style>
 
 <script>
 import Nav from '@/components/Nav.vue';
+import Project from '@/components/Project.vue';
+import IN from '@/assets/projects/project.png';
+import OFN from '@/assets/projects/ofn.png';
 
 export default {
   name: 'Projects',
   components: {
     Nav,
+    Project,
   },
+  data: () => ({
+    projects: [
+      {
+        src: IN,
+        title: 'Inveractive Narrative',
+      },
+      {
+        src: OFN,
+        title: 'Openfinance',
+      },
+    ],
+  }),
 };
 </script>
